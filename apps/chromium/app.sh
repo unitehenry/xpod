4aa256a4db1972cfca285c974bd6d57d383b97f2
Henry Unite <unitehenry@gmail.com>
2026-07-26 01:02:00 +0000

    chore: cdp port

--- apps/chromium/app.sh
+++ apps/chromium/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-socat TCP-LISTEN:9222,bind=0.0.0.0,reuseaddr,fork TCP6:[::1]:9222 &
+socat TCP-LISTEN:9222,bind=0.0.0.0,reuseaddr,fork TCP:127.0.0.1:9223 &
 
 mkdir -p /var/lib/chromium
 chmod -R 777 /var/lib/chromium
@@ -10,4 +10,4 @@ rm -rf /var/lib/chromium/profile/Singleton*
 CHROMIUM_WIDTH=${RESOLUTION%%x*}
 CHROMIUM_HEIGHT=${RESOLUTION#*x}; CHROMIUM_HEIGHT=${CHROMIUM_HEIGHT%%x*}
 
-su -c "chromium --disable-gpu --start-maximized --window-size=${CHROMIUM_WIDTH},${CHROMIUM_HEIGHT} --force-device-scale-factor=1 --remote-debugging-port=9222 --remote-debugging-address=0.0.0.0 --user-data-dir=/var/lib/chromium/profile" chrome
+su -c "chromium --disable-gpu --start-maximized --window-size=${CHROMIUM_WIDTH},${CHROMIUM_HEIGHT} --force-device-scale-factor=1 --remote-debugging-port=9223 --user-data-dir=/var/lib/chromium/profile" chrome
